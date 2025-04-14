9ed5a04bfd6063c9629b2309ee8afeeb9800da94
Colin Le <Colin.Le@ucf.edu>
2025-04-14 16:13:52 -0400

    Test code for tokenizer and parser

--- main.go
+++ main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"github.com/colintle/BrainForge/compiler"
 )
 
-func main(){
-	fmt.Println("Hello")
+func main() {
+	input := "++>++[<+>-]."
+
+	tokens := compiler.Tokenizer(input)
+
+	fmt.Println("Tokens:")
+	for _, token := range tokens {
+		fmt.Println(token.ToString())
+	}
+
+	ast := compiler.Parser(tokens)
+	fmt.Println("\nAST:")
+	for _, node := range ast {
+		fmt.Println(node.ToString())
+	}
 }

23162f988ab0b04cf905eb05edec7aa3c1e7fb43
Colin Le <Colin.Le@ucf.edu>
2025-04-14 16:13:24 -0400

    Make to string for ast node

--- models/bf/node.go
+++ models/bf/node.go
@@ -1,6 +1,26 @@
 package bf
 
+import (
+	"strings"
+	"fmt"
+)
+
 type BfNode struct{
 	Type BfType
 	Body []*BfNode
+}
+
+func (node *BfNode) ToString() string {
+	var builder strings.Builder
+	node.buildString(&builder, 0)
+	return builder.String()
+}
+
+func (node *BfNode) buildString(builder *strings.Builder, level int) {
+	indent := strings.Repeat("  ", level)
+	builder.WriteString(fmt.Sprintf("%sNode Type: %s\n", indent, bfTypeToString(node.Type)))
+
+	for _, child := range node.Body {
+		child.buildString(builder, level+1)
+	}
 }

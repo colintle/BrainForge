a5fd8e5e8ffc350e3ceca3c1bfe49188c9367437
Colin Le <Colin.Le@ucf.edu>
2025-04-23 13:46:52 -0400

    Start of code generator

--- compiler/code_generator.go
+++ compiler/code_generator.go
@@ -1,1 +1,12 @@
 package compiler
+
+import (
+	"strings"
+	"github.com/colintle/BrainForge/models/golang"
+)
+
+func CodeGenerator(ast []*golang.GoNode) string {
+	var builder strings.Builder
+
+	return builder.String()
+}

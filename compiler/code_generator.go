7a19fdf35abb48ac1ca65a9243555a7aab0e9b81
Colin Le <Colin.Le@ucf.edu>
2025-04-23 14:57:50 -0400

    add starter statements for go

--- compiler/code_generator.go
+++ compiler/code_generator.go
@@ -7,6 +7,17 @@ import (
 
 func CodeGenerator(ast []*golang.GoNode) string {
 	var builder strings.Builder
+	builder.WriteString("package main\n\n")
+	builder.WriteString("import (\n\t\"fmt\"\n)\n\n")
+	builder.WriteString("func main() {\n")
+	builder.WriteString("\tvar memory [30000]byte\n")
+	builder.WriteString("\tvar pointer int\n\n")
+	builder.WriteString("\tvar input byte \n\n")
+	builder.WriteString("\t_ = input \t")
+
+	// traverse ast
+	
+	builder.WriteString("}\n")
 
 	return builder.String()
 }

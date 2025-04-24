8042eb6e67b6249f9f360d680c6130224fd87ae7
Colin Le <Colin.Le@ucf.edu>
2025-04-24 10:28:55 -0400

    Tranverse through go ast to generate the corresponding go code

--- compiler/code_generator.go
+++ compiler/code_generator.go
@@ -2,8 +2,30 @@ package compiler
 
 import (
 	"strings"
+	"fmt"
 	"github.com/colintle/BrainForge/models/golang"
 )
+
+func generateCode(builder *strings.Builder, node *golang.GoNode, level int) {
+	indent := strings.Repeat("\t", level)
+
+	switch node.Type {
+	case golang.GoStatement:
+		builder.WriteString(fmt.Sprintf("%s%s\n", indent, node.Statement))
+
+	case golang.GoFor:
+		builder.WriteString(fmt.Sprintf("%sfor %s {\n", indent, node.Condition))
+		for _, child := range node.Body {
+			generateCode(builder, child, level+1)
+		}
+		builder.WriteString(fmt.Sprintf("%s}\n", indent))
+
+	case golang.GoProgram:
+		for _, child := range node.Body {
+			generateCode(builder, child, level)
+		}
+	}
+}
 
 func CodeGenerator(ast []*golang.GoNode) string {
 	var builder strings.Builder
@@ -11,12 +33,15 @@ func CodeGenerator(ast []*golang.GoNode) string {
 	builder.WriteString("import (\n\t\"fmt\"\n)\n\n")
 	builder.WriteString("func main() {\n")
 	builder.WriteString("\tvar memory [30000]byte\n")
-	builder.WriteString("\tvar pointer int\n\n")
-	builder.WriteString("\tvar input byte \n\n")
-	builder.WriteString("\t_ = input \t")
+	builder.WriteString("\tvar pointer int\n")
+	builder.WriteString("\tvar input byte \n")
+	builder.WriteString("\t_ = input \n\n")
 
 	// traverse ast
-	
+	for _, node := range ast {
+		generateCode(&builder, node, 1)
+	}
+
 	builder.WriteString("}\n")
 
 	return builder.String()
